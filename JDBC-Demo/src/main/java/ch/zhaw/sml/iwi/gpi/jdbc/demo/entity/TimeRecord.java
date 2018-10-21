94ceaf7059b138c2171dd28320b0b757970ff74a
Peter Heinrich <peter.heinrich@its-heinrich.ch>
2018-10-21 10:20:01 +0200

    initial commit

---
+++ JDBC-Demo/src/main/java/ch/zhaw/sml/iwi/gpi/jdbc/demo/entity/TimeRecord.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.zhaw.sml.iwi.gpi.jdbc.demo.entity;
+
+/**
+ *
+ * @author peterhe
+ */
+public class TimeRecord {
+
+}
