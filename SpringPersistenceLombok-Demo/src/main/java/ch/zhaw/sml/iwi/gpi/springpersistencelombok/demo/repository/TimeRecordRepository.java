0a6404f9ce652c7588d8c3afac64472c3984f58c
Björn Scheppler <scep@zhaw.ch>
2018-10-23 09:55:51 +0200

    Crud to JpaRepository

--- SpringPersistenceLombok-Demo/src/main/java/ch/zhaw/sml/iwi/gpi/springpersistencelombok/demo/repository/TimeRecordRepository.java
+++ SpringPersistenceLombok-Demo/src/main/java/ch/zhaw/sml/iwi/gpi/springpersistencelombok/demo/repository/TimeRecordRepository.java
@@ -2,11 +2,11 @@ package ch.zhaw.sml.iwi.gpi.springpersistencelombok.demo.repository;
 
 import ch.zhaw.sml.iwi.gpi.springpersistencelombok.demo.entity.Employee;
 import ch.zhaw.sml.iwi.gpi.springpersistencelombok.demo.entity.TimeRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 
-public interface TimeRecordRepository extends CrudRepository<TimeRecord,Long> {
+public interface TimeRecordRepository extends JpaRepository<TimeRecord,Long> {
     @Query("SELECT T FROM TimeRecord AS T WHERE T.employee.name = ?1")
     public Iterable<TimeRecord> findAllByName(String name);
         
